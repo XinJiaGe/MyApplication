f59b3f81326b9ca21c0814bebf133ef877ce98f7
lixinjia <347292753@qq.com>
2017-06-20 16:39:57 +0800

    ss

---
+++ app/src/main/java/com/adaptation/lixinjia/myapplication/activity/CtextActivity.kt
@@ -0,0 +1,12 @@
+package com.adaptation.lixinjia.myapplication.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CtextActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ctext)
+    }
+}
